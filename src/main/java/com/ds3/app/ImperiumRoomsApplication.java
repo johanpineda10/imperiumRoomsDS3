9c31f7e9265487fec7596c137108cdf91edd7a0a
Sebastianlaverde <slaverdeiesvgmail.com>
2025-12-07 16:08:08 -0500

    Project imperiums rooms

---
+++ src/main/java/com/ds3/app/ImperiumRoomsApplication.java
@@ -0,0 +1,13 @@
+package com.ds3.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImperiumRoomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImperiumRoomsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ds3/app/ImperiumRoomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds3.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImperiumRoomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
